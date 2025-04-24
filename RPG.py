621fc7a69de7b97b84a36cda1d1ea9bcbb441bab
Noah Thao <tsimeng@hotmail.com>
2025-04-23 19:46:48 -0700

    The Framework

---
+++ RPG.py
@@ -0,0 +1,12 @@
+
+
+def main(debug=False):
+    from textx import metamodel_fromfile
+    rPG_m = metamodel_fromfile('') #Insert grammar file here
+    rPG_model = robot_m.model_from_file('') #Insert program file here
+
+    #Register object processors here 
+
+    #Place object and "interpreter" here
+
+
