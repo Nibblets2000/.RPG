824e03bd8530fa687491da5df810f324dccf0415
Nibblets2000 <tsimeng@hotmail.com>
2025-04-30 00:53:00 -0700

    The Framework

--- RPG.py
+++ RPG.py
@@ -1,12 +1,66 @@
 
 
+
+#Math Parser
+def eval_expr(expr):
+    if '-' in expr:
+        left, right = expr.split('-')
+        left = expr(left)
+        right = eval_expr(right)
+        ans = left - right
+    elif '+' in expr:
+        left, right = expr.split('+')
+        left = eval_expr(left)
+        right = eval_expr(right)
+        ans = left + right
+    elif '/' in expr:
+        left, right = expr.split('/')
+        left = eval_expr(left)
+        right = eval_expr(right)
+        ans = left / right
+    elif '*' in expr:
+        left, right = expr.split('*')
+        left = eval_expr(left)
+        right = eval_expr(right)
+        ans = left * right
+    elif '^' in expr:
+        left, right = expr.split('^')
+        left = eval_expr(left)
+        right = eval_expr(right)
+        ans = left ** right
+    else:
+        return (float(expr))
+    return expr
+
+def varmap(var, s):
+    return s[var]
+
+def eval_var(var, s):
+    if var in s:
+        return varmap(var, s)
+    else:
+        raise Exception(f"Variable '{var}' not defined")
+
+#Interpreter
+def interpret(self, model):
+    for c in model.commands:
+        if c.__class__.__name__ == "":
+            return
+        elif c.__class__.__name__ == "":
+            return
+        else:
+            return
+
+
+
 def main(debug=False):
     from textx import metamodel_fromfile
-    rPG_m = metamodel_fromfile('') #Insert grammar file here
-    rPG_model = robot_m.model_from_file('') #Insert program file here
+    rPG_m = metamodel_fromfile('RPG.tx') #Insert grammar file here
+    rPG_model = rPG_m.model_from_file('Program.RPG') #Insert program file here
+    
+    interpret(rPG_model)
 
-    #Register object processors here 
-
-    #Place object and "interpreter" here
+if __name__ == "__main__":
+    main()
 
 
